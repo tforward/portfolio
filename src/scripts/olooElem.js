cff5e392b7b8434331433d90c4acbe0772dfd770
Tristan Forward <tforward@users.noreply.github.com>
2018-07-21 21:32:55 -0400

    layout

--- src/scripts/olooElem.js
+++ src/scripts/olooElem.js
@@ -1,81 +1,33 @@
 // ======================================================================
-// Element Delegator
+// Element
 // ======================================================================
 
-export function ElementDelegator(proto = null) {
-  // This is the base Delegator "Class" for a element
-  // By default does not inherirt the proto chain from another object
-  const Element = Object.create(proto);
-  // Use "create" for creating a new element
-  Element.create = function create(elemId, type) {
-    this.elem = document.createElement(type);
-    this.elem.id = elemId;
-    Element.props(elemId);
-    Element.gc(["init", "create"]);
-  };
-  // Use "init" when working with an existing element
-  Element.init = function init(elemId) {
-    this.elem = document.getElementById(elemId);
-    if (this.elem === null) {
-      console.log(`Init Error: No element with id: "${elemId}"`);
-    }
-    Element.props(elemId);
-    // After creation garbage collected init/create
-    Element.gc(["init", "create"]);
-  };
-  Element.props = function props(elemId) {
-    this.id = elemId;
-    this.toggle = 0;
-  };
-  // Garbage Collect
-  // After a element is created or init we can delete these properties as
-  // they will no longer be used in the life cycles of the Element
-  Element.gc = function gc(items) {
-    items.forEach(item => {
-      delete Element[item];
+export function ElemDelegator(state) {
+  const Content = Object.create(null);
+
+  Content.createElems = function createElems() {
+    Object.keys(state).forEach(key => {
+      state[key].elem = document.createElement(state[key].elem);
+      // For assigning values to node elements
+      Object.keys(state[key]).forEach(i => {
+        state[key].elem[i] = state[key][i];
+      });
     });
-    // Delete gc itself
-    // delete Element.gc;
   };
-  return Element;
+  return Content;
 }
 
 // ======================================================================
-// Delegated Element Utilities
+//  Element Utilities
 // ======================================================================
 
-export function initElemObjects(ids, elemType, delegator, proto = null, create = true) {
-  // Used for quickly initiating Element delegated objects
-  //  delegator: The delegator to assign to the returned object
-  //  proto: [Optional] The prototype to give the object
-  //  Can accept another proto delegator
-  //  Typically good idea to assign the return object to the Subscribers.addObjs property
-  const objs = [];
-  let obj;
-  ids.forEach(id => {
-    if (proto === null) {
-      obj = delegator();
-    } else {
-      obj = delegator(proto());
-    }
-    if (create) {
-      obj.create(id, elemType);
-    } else {
-      obj.init(id);
-    }
-    objs[id] = obj;
-  });
-  return objs;
-}
+export function FragmentDelegator() {
+  const FragmentDele = Object.create(null);
 
-export function FragmentDelegator(proto = null) {
-  const Fragment = Object.create(proto);
-
-  Fragment.initFragment = function initFragment() {
-    this.fragment = document.createDocumentFragment();
+  FragmentDele.addFragments = function addFragments(items) {
+    const fragment = document.createDocumentFragment();
+    items.forEach(item => fragment.appendChild(item.elem));
+    return fragment;
   };
-  Fragment.addNodes = function addNodes(items) {
-    items.forEach(item => this.fragment.appendChild(item.elem));
-  };
-  return Fragment;
+  return FragmentDele;
 }
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,7 +5,7 @@ import normalise_ from "../../node_modules/normalize.css";
 import css_ from "../css/styles.css";
 import { EventDelegator, getTargetId } from "./olooEvent";
 import { SubscribersDelegator } from "./olooObserver";
-import { ElementDelegator, initElemObjects } from "./olooElem";
+import { ElemDelegator, FragmentDelegator } from "./olooElem";
 import leaflet_ from "./leafletembed";
 
 const myBase = Object.create(null);
@@ -14,58 +14,31 @@ const myApp = SubscribersDelegator();
 myBase.initApplication = function init() {
   myApp.init();
 
-  const createCard = function card(id) {
+  const createCard = function card(elemId) {
+    // Just use the same syntax for node elements
     const state = {
-      id,
-      elem: document.createElement("div"),
-      title: document.createElement("h2"),
-      desc: document.createElement("p"),
-      data: Object.create(null)
+      card: { elem: "div", id: elemId, className: "card pad1 boxShadow" },
+      title: { elem: "h2", textContent: "Calculator", className: "card-title pad1" },
+      img: { elem: "img", className: "card-img", src: "../images/calculator.jpg", alt: "A Calculator" },
+      desc: { elem: "p", textContent: "A calculator build with love", className: "card-desc pad1" }
     };
+    const addContent = () => ({
+      appendContent: function appendContent(parent) {
+        const content = this.addFragments([this.title, this.img, this.desc]);
+        this.card.elem.appendChild(content);
+        parent.appendChild(this.card.elem);
+      }
+    });
     // If you add an object to create it will show up on the __proto__
     // for that object, default null.
-    return Object.assign(Object.create(state), contentManager(state));
+    return Object.assign(Object.create(state), addContent(), ElemDelegator(state), FragmentDelegator(state));
   };
 
-  function contentManager(state, proto = null) {
-    const Content = Object.create(proto);
-    Content.setText = (elem, value) => {
-      elem.textContent = value;
-    };
-    Content.setClass = (elem, classname) => {
-      elem.className = classname;
-    };
-    return Content;
-  }
-
-  function defineCard({ _card, _title, _desc }) {
-    createElems({ _card, _title, _desc });
-    const card = createCard("card-calc");
-    card.elem.className = _card.classname;
-
-    card.title.textContent = _title.textContent;
-    card.title.className = _title.classname;
-
-    card.desc.textContent = _desc.textContent;
-    return card;
-  }
-
-  const calcCard = {
-    _card: { elem: "div", classname: "card pad1 boxShadow" },
-    _title: { elem: "h2", textContent: "Calculator", classname: "card-title pad1" },
-    _desc: { elem: "p", textContent: "A calculator build with love", classname: "card-desc pad1" }
-  };
-
-  const x = defineCard(calcCard);
+  const x = createCard("card-calc");
+  x.createElems();
 
   const projects = document.getElementById("projects");
-  projects.appendChild(x.elem);
-  x.elem.appendChild(x.title);
-  x.elem.appendChild(x.desc);
-
-  function createElems(...args) {
-    console.log(...args);
-  }
+  x.appendContent(projects);
 
   //   const eventSandbox = EventDelegator();
   //   eventSandbox.initEvent("eventSandbox", "click", { tags: ["BUTTON"] });
