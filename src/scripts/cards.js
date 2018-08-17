46a5680ff1ea8ef8156078e7f4de499e7525943b
Tristan Forward <tforward@users.noreply.github.com>
2018-08-16 21:15:52 -0400

    cards seperate

---
+++ src/scripts/cards.js
@@ -0,0 +1,119 @@
+import { ElemDelegator, FragmentDelegator } from "./olooElem";
+
+const createCard = function card(data) {
+  // Just use the same syntax for node elements
+  const state = `<div id="${data.elemId}" class="card ${data.cardClass}">
+        <div class="card-thumb ${data.cardThumbSize}" style="background-image: url(../images/${data.imageName}.jpg)">
+        <a class="card-content" href="${data.demoUrl}" target="_blank">
+          <div class="card-thumb-overlay center">
+              <div class="card-btn-holder center">
+    <a class="noTextDecoration" href="${
+      data.demoUrl
+    }" target="_blank"><button class="card-btn demo center noSelect">Demo</button></a><a class="noTextDecoration" href="${
+    data.codeUrl
+  }" target="_blank"><button class="card-btn code center noSelect">Code</button></a></div></div></div>
+    </a>
+    <article class="card-article">
+        <h3 class="card-title">${data.title}</h3>
+        <span class="card-toolbox">${data.tools}</span>
+    </article>
+  </div>`;
+
+  // If you add an object to create it will show up on the __proto__
+  // for that object, default null.
+  return Object.assign(Object.create(null), ElemDelegator(state), FragmentDelegator(state));
+};
+
+// TODO Have these on a seperate page and import them
+// TODO item-1 classname thing
+//     // Need image cover
+
+export function defineCards() {
+  const card = Object.create(null);
+
+  card.cardCalc = createCard({
+    elemId: "card-calc",
+    title: "Calculator",
+    description: "A Responsive Formula Expression Logic Calculator",
+    demoUrl: "https://tforward.github.io/JSCalculator/",
+    codeUrl: "https://github.com/tforward/JSCalculator",
+    tools: "HTML, CSS (Grid), JS (ESLint)",
+    imageName: "calculator",
+    imgClass: "card-img-portrait",
+    cardClass: "card"
+  });
+
+  card.cardTwitch = createCard({
+    elemId: "card-twitch",
+    title: "Twitch Viewer",
+    description: "A Twitch API Responsive Viewer",
+    demoUrl: "https://tforward.github.io/fccTwitchTV/",
+    codeUrl: "https://github.com/tforward/fccTwitchTV",
+    tools: "HTML, CSS (Flexbox), JS (ESLint, Webpack, Twitch API)",
+    imageName: "twitch",
+    imgClass: "card-img-landscape",
+    cardClass: "card"
+  });
+
+  card.cardPomodoro = createCard({
+    elemId: "card-pomodoro",
+    title: "Pomodoro Clock",
+    description: "A Responsive Pomodoro Clock",
+    demoUrl: "https://tforward.github.io/fccPomodoro/",
+    codeUrl: "https://github.com/tforward/fccPomodoro",
+    imageName: "pomodoro",
+    imgClass: "card-img-portrait",
+    tools: "HTML, CSS (Flexbox, Grid, Variables), JS (ESLint, Webpack)",
+    cardClass: "card"
+  });
+
+  card.cardGridImage = createCard({
+    elemId: "card-gridImage",
+    title: "Grid 5x5",
+    description: "A just for fun experiment with CSS Grid",
+    demoUrl: "https://tforward.github.io/gridImage/",
+    codeUrl: "https://github.com/tforward/gridImage",
+    imageName: "grid5x5",
+    imgClass: "card-img-portrait",
+    tools: "HTML, CSS (Flexbox, Grid), JS (ESLint, Webpack)",
+    cardClass: "card"
+  });
+
+  card.cardSimon = createCard({
+    elemId: "card-simon",
+    title: "Simon Game",
+    description: "The classic 1980's game Simon",
+    demoUrl: "https://tforward.github.io/fccSimon/",
+    codeUrl: "https://github.com/tforward/fccSimon",
+    imageName: "simon",
+    imgClass: "card-img-portrait",
+    tools: "HTML, CSS (Flexbox), JS (ESLint, Webpack)",
+    cardClass: "card"
+  });
+
+  card.cardTicTac = createCard({
+    elemId: "card-tictac",
+    title: "Tic Tac Toe Game",
+    description: "The always classic Tic Tac Toe!",
+    demoUrl: "https://tforward.github.io/fccTicTacToe/",
+    codeUrl: "https://github.com/tforward/fccTicTacToe",
+    imageName: "tictac",
+    imgClass: "card-img-portrait",
+    tools: "HTML, CSS (Flexbox), JS (ESLint, Gulp)",
+    cardClass: "card"
+  });
+
+  card.cardLeafshoot = createCard({
+    elemId: "card-leaf",
+    title: "LeafShoot",
+    description: "A responsive map template",
+    demoUrl: "https://tforward.github.io/LeafShoot/",
+    codeUrl: "https://github.com/tforward/LeafShoot",
+    imageName: "leafshoot",
+    imgClass: "card-img-landscape",
+    tools: "HTML, CSS (Bootstrap), JS (Leaflet)",
+    cardClass: "card"
+  });
+
+  return card;
+}
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,6 +6,7 @@ import css_ from "../css/styles.css";
 import { EventDelegator, getTargetId } from "./olooEvent";
 import { SubscribersDelegator } from "./olooObserver";
 import { ElemDelegator, FragmentDelegator } from "./olooElem";
+import { defineCards } from "./cards";
 import leaflet_ from "./leafletembed";
 
 const myBase = Object.create(null);
@@ -14,133 +15,26 @@ const myApp = SubscribersDelegator();
 myBase.initApplication = function init() {
   myApp.init();
 
-  const createCard = function card(data) {
-    // Just use the same syntax for node elements
-    const state = `<div id="${data.elemId}" class="card">
-    <a class="card-content" href="${data.demoUrl}">
-        <div class="card-thumb" style="background-image: url(../images/${data.imageName}.jpg)">
-          <div class="card-thumb-overlay center">
-              <div class="card-btn-holder center">
-    <a class="noTextDecoration" href="${
-      data.demoUrl
-    }" target="_blank"><button class="card-btn demo center noSelect">Demo</button></a><a class="noTextDecoration" href="${
-      data.codeUrl
-    }" target="_blank"><button class="card-btn code center noSelect">Code</button></a></div></div></div>
-    <article>
-        <h3 class="title">${data.title}</h3>
-        <span class="toolbox">${data.tools}</span>
-    </article>
-    </a>
-  </div>`;
-
-    // If you add an object to create it will show up on the __proto__
-    // for that object, default null.
-    return Object.assign(Object.create(null), ElemDelegator(state), FragmentDelegator(state));
-  };
+  const card = defineCards();
 
   // TODO Have these on a seperate page and import them
   // TODO item-1 classname thing
   //     // Need image cover
   const projects = document.getElementById("projects");
-  const cardCalc = createCard({
-    elemId: "card-calc",
-    title: "Calculator",
-    description: "A Responsive Formula Expression Logic Calculator",
-    demoUrl: "https://tforward.github.io/JSCalculator/",
-    codeUrl: "https://github.com/tforward/JSCalculator",
-    tools: "HTML, CSS (Grid), JS (ESLint)",
-    imageName: "calculator",
-    imgClass: "card-img-portrait",
-    cardClass: "card"
-  });
-  cardCalc.addHtml(projects);
+  const projectsContainer = document.createElement("div");
+  projectsContainer.className = "projects-container";
 
-  const cardTwitch = createCard({
-    elemId: "card-twitch",
-    title: "Twitch Viewer",
-    description: "A Twitch API Responsive Viewer",
-    demoUrl: "https://tforward.github.io/fccTwitchTV/",
-    codeUrl: "https://github.com/tforward/fccTwitchTV",
-    tools: "HTML, CSS (Flexbox), JS (ESLint, Webpack, Twitch API)",
-    imageName: "twitch",
-    imgClass: "card-img-landscape",
-    cardClass: "card cardSpan2"
-  });
-  cardTwitch.addHtml(projects);
+  card.cardCalc.addHtml(projectsContainer);
+  card.cardTwitch.addHtml(projectsContainer);
+  card.cardPomodoro.addHtml(projectsContainer);
+  card.cardGridImage.addHtml(projectsContainer);
+  card.cardSimon.addHtml(projectsContainer);
+  card.cardTicTac.addHtml(projectsContainer);
+  card.cardLeafshoot.addHtml(projectsContainer);
 
-  const cardPomodoro = createCard({
-    elemId: "card-pomodoro",
-    title: "Pomodoro Clock",
-    description: "A Responsive Pomodoro Clock",
-    demoUrl: "https://tforward.github.io/fccPomodoro/",
-    codeUrl: "https://github.com/tforward/fccPomodoro",
-    imageName: "pomodoro",
-    imgClass: "card-img-portrait",
-    tools: "HTML, CSS (Flexbox, Grid, Variables), JS (ESLint, Webpack)",
-    cardClass: "card"
-  });
-  cardPomodoro.addHtml(projects);
-
-  const cardGridImage = createCard({
-    elemId: "card-gridImage",
-    title: "Grid 5x5",
-    description: "A just for fun experiment with CSS Grid",
-    demoUrl: "https://tforward.github.io/gridImage/",
-    codeUrl: "https://github.com/tforward/gridImage",
-    imageName: "grid5x5",
-    imgClass: "card-img-portrait",
-    tools: "HTML, CSS (Flexbox, Grid), JS (ESLint, Webpack)",
-    cardClass: "card"
-  });
-  cardGridImage.addHtml(projects);
-
-  const cardSimon = createCard({
-    elemId: "card-simon",
-    title: "Simon Game",
-    description: "The classic 1980's game Simon",
-    demoUrl: "https://tforward.github.io/fccSimon/",
-    codeUrl: "https://github.com/tforward/fccSimon",
-    imageName: "simon",
-    imgClass: "card-img-portrait",
-    tools: "HTML, CSS (Flexbox), JS (ESLint, Webpack)",
-    cardClass: "card"
-  });
-  cardSimon.addHtml(projects);
-
-  const cardTicTac = createCard({
-    elemId: "card-tictac",
-    title: "Tic Tac Toe Game",
-    description: "The always classic Tic Tac Toe!",
-    demoUrl: "https://tforward.github.io/fccTicTacToe/",
-    codeUrl: "https://github.com/tforward/fccTicTacToe",
-    imageName: "tictac",
-    imgClass: "card-img-portrait",
-    tools: "HTML, CSS (Flexbox), JS (ESLint, Gulp)",
-    cardClass: "card"
-  });
-  cardTicTac.addHtml(projects);
-
-  const cardLeafshoot = createCard({
-    elemId: "card-leaf",
-    title: "LeafShoot",
-    description: "A responsive map template",
-    demoUrl: "https://tforward.github.io/LeafShoot/",
-    codeUrl: "https://github.com/tforward/LeafShoot",
-    imageName: "leafshoot",
-    imgClass: "card-img-landscape",
-    tools: "HTML, CSS (Bootstrap), JS (Leaflet)",
-    cardClass: "card"
-  });
-  cardLeafshoot.addHtml(projects);
+  projects.appendChild(projectsContainer);
 
   // TODO Create a function to semi randomly order the projects or similar
-  // cardPomodoro.appendContent(projects);
-  // cardCalc.appendContent(projects);
-  // cardTwitch.appendContent(projects);
-  // cardGridImage.appendContent(projects);
-  // cardSimon.appendContent(projects);
-  // cardTicTac.appendContent(projects);
-  // cardLeafshoot.appendContent(projects);
 
   // Band for differnet content (LATER)
 
