9518c24dca5e2c9edde75c385418c5e4e25fb436
Tristan Forward <tforward@users.noreply.github.com>
2018-11-20 21:09:10 -0500

    mapwork

--- src/scripts/leafletembed.js
+++ src/scripts/leafletembed.js
@@ -23,3 +23,40 @@ function initmap() {
 }
 
 initmap();
+
+function getLeafListPts(pointList) {
+  const leafListPts = [];
+  pointList.forEach(i => {
+    const p = new L.LatLng(i[0], i[1]);
+    leafListPts.push(p);
+  });
+  return leafListPts;
+}
+
+function panTimer(points) {
+  let sec = 0;
+  setInterval(() => {
+    if (sec < points.length) {
+      map.panTo(points[sec]);
+      sec += 1;
+    }
+  }, 1300);
+}
+
+function randomIntMinMax(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const x1 = randomIntMinMax(46, 68);
+const y1 = randomIntMinMax(-56, -139);
+
+console.log(x1);
+console.log(y1);
+
+// const pointList = [[44.331223, -76.235374], [44.329787, -76.252932], [44.327638, -76.275248], [44.325579, -76.302456]];
+
+const pointList = [[x1, y1]];
+
+const leafList = getLeafListPts(pointList);
+
+panTimer(leafList);
