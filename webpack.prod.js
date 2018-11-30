781d28362cff1ff5459d718a16ae5344e5173387
Tristan Forward <tforward@users.noreply.github.com>
2018-11-29 19:17:21 -0500

    prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -35,6 +35,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-inline-loader"
       }
     ]
   }
